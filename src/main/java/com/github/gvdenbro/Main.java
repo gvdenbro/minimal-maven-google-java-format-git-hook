34888530803cf840d17a9d886573174f3e2b0e92
gerg <gregory.vandenbroucke@gmail.com>
2025-03-21 16:34:56 +0100

    test reformatting

---
+++ src/main/java/com/github/gvdenbro/Main.java
@@ -0,0 +1,8 @@
+package com.github.gvdenbro;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Add wrong format and see that on commit it gets reformatted!");
+  }
+}
